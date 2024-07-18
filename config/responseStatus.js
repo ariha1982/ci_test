e094de9c0a2710e4624ff37faa3f9b234bf0926a
Jayde1012 <ariha1982@naver.com>
2024-07-18 14:07:23 +0900

    변화 test

--- config/responseStatus.js
+++ config/responseStatus.js
@@ -5,4 +5,5 @@ export const status = {
     SUCCESS: {status: StatusCodes.OK, "isSuccess": true, "code": 200, "message": "success!"},
     NOT_FOUND: {status: StatusCodes.NOT_FOUND, "isSuccess": false, "code": 400, "message": "not found"},
     DB_ERROR: {status: StatusCodes.INTERNAL_SERVER_ERROR, "isSuccess": false, "code": 500, "message": "서버 내부 에러. 관리자에게 문의하세요."},
+    DB_ERROR2: {status: StatusCodes.INTERNAL_SERVER_ERROR, "isSuccess": false, "code": 500, "message": "서버 내부 에러. 관리자에게 문의하세요!"},
 }
